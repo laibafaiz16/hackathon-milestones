4cc5502d2f55ed38b64ca64062f842883af704b4
laibafaiz16 <laibafaiz.16@gmail.com>
2024-09-15 02:07:44 +0500

    Add files via upload

---
+++ milestone1/script.ts
@@ -0,0 +1,12 @@
+// script.ts
+const skillsSection = document.querySelector('#skills');
+const toggleSkillsButton = document.querySelector('#toggle-skills-button');
+
+toggleSkillsButton.addEventListener('click', () => {
+    skillsSection.classList.toggle('hidden');
+});
+
+
+
+
+
